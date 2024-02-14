2d3fe6cd0f0a41ce221b39cbdc836782b211dcce
Omar <omar.banoun@yahoo.fr>
2024-02-14 04:54:48 +0100

    backoffice update - home images

--- src/admin/Custom/CustomPage.js
+++ src/admin/Custom/CustomPage.js
@@ -11,22 +11,44 @@ const [companyName, setCompanyName] = useState('');
 const [themePrimaryColor, setThemePrimaryColor] = useState('#ffb700');
 const [themeSecondaryColor, setThemeSecondaryColor] = useState('#151313');
 const [image, setImage] = useState(null);
-const [homeImg, setHomeImg] = useState(null);
+const [homeImgs, setHomeImgs] = useState([{ file: null, url: '' }]);
 const [tempImageUrl, setTempImageUrl] = useState(null);
-const [tempHomeImgUrl, setTempHomeImgUrl] = useState(null);
-const handleFileChange = (e, type) => {
-    if (e.target.files && e.target.files[0]) {
-        const file = e.target.files[0];
+const [tempHomeImgUrls, setTempHomeImgUrls] = useState([]);
+
+const handleFileChange = (e, type, index) => {
+    if (e.target.files && e.target.files.length > 0) {
         if (type === 'logo') {
-            setImage(file); // Met à jour l'état de l'image du logo
-            setTempImageUrl(URL.createObjectURL(file)); // Prévisualisation du logo
-        } else if (type === 'homeImg') {
-            setHomeImg(file); // Met à jour l'état de l'image de homeImg
-            setTempHomeImgUrl(URL.createObjectURL(file)); // Prévisualisation de homeImg
+            const file = e.target.files[0];
+            if (tempImageUrl) URL.revokeObjectURL(tempImageUrl);
+            setImage(file);
+            setTempImageUrl(URL.createObjectURL(file));
+        } else if (type === 'homeImgs') {
+            // Traitement pour l'ajout de nouvelles images ou le remplacement d'une image spécifique
+            if (index !== undefined) {
+                // Remplacement d'une image spécifique
+                const file = e.target.files[0];
+                const newUrl = URL.createObjectURL(file);
+                setHomeImgs(currentImgs => {
+                    const newImgs = [...currentImgs];
+                    if (newImgs[index]?.url) URL.revokeObjectURL(newImgs[index].url);
+                    newImgs[index] = { file, url: newUrl };
+                    return newImgs;
+                });
+                setTempHomeImgUrls(currentUrls => {
+                    const updatedUrls = [...currentUrls];
+                    updatedUrls[index] = newUrl;
+                    return updatedUrls;
+                });
+            } else {
+                // Ajout de nouvelles images
+                const newFiles = Array.from(e.target.files);
+                const newImgs = newFiles.map(file => ({ file, url: URL.createObjectURL(file) }));
+                setHomeImgs(currentImgs => [...currentImgs, ...newImgs]);
+                setTempHomeImgUrls(currentUrls => [...currentUrls, ...newImgs.map(img => img.url)]);
+            }
         }
     }
 };
-
 
 useEffect(() => {
     axios.get('https://maro.alwaysdata.net/api/settings')
@@ -34,7 +56,12 @@ useEffect(() => {
             console.log('response.data:', response.data);
             const imgData = response.data.length > 0 ? response.data[0] : {};
             setTempImageUrl(imgData.logo ? `https://maro.alwaysdata.net/${imgData.logo}` : null);
-            setTempHomeImgUrl(imgData.homeImg ? `https://maro.alwaysdata.net/${imgData.homeImg}` : null);
+            if (imgData.homeImgs && Array.isArray(imgData.homeImgs)) {
+                const homeImgUrls = imgData.homeImgs.map(img => `https://maro.alwaysdata.net/${img}`);
+                setTempHomeImgUrls(homeImgUrls);
+            } else {
+                setTempHomeImgUrls([]);
+            }
             setThemePrimaryColor(response.data[0].themePrimaryColor);
             setThemeSecondaryColor(response.data[0].themeSecondaryColor);
             setCompanyName(response.data[0].companyName);
@@ -46,9 +73,9 @@ const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append('logo', image); // Ajoute le logo
-    formData.append('homeImg', homeImg);
-    // if (homeImg) formData.append('homeImg', homeImg);
-    // Ajoute également les couleurs primaires et secondaires au formData
+    homeImgs.forEach(({ file }) => {
+        if (file) formData.append('homeImgs', file); // Assurez-vous d'ajouter le fichier et non l'objet
+    });
     formData.append('themePrimaryColor', themePrimaryColor);
     formData.append('themeSecondaryColor', themeSecondaryColor);
     formData.append('companyName', companyName);
@@ -59,12 +86,14 @@ const handleSubmit = async (e) => {
                 'Content-Type': 'multipart/form-data',
             },
         });
-        console.log('Logo et couleurs mis à jour avec succès');
-        window.location.reload(); // Ou une meilleure façon de rafraîchir les données affichées
+        console.log('Mise à jour réussie');
+        // Mettez à jour l'état de votre application pour refléter les changements au lieu de recharger la page
+        window.location.reload();
     } catch (error) {
-        console.error("Erreur lors de la mise à jour du logo et des couleurs", error);
+        console.error("Erreur lors de la mise à jour", error);
     }
 };
+
 
 const handlePrimaryColorChange = (event) => {
     setThemePrimaryColor(event.target.value);
@@ -77,6 +106,11 @@ const handlePrimaryColorChange = (event) => {
   const handleCompanyNameChange = (event) => {
     setCompanyName(event.target.value);
   }
+
+const addNewImage = () => {
+    setHomeImgs([...homeImgs, { file: null, url: '' }]);
+};
+
 
   const saveColorChanges = () => {
     axios.put('https://maro.alwaysdata.net/api/settings', {
@@ -94,14 +128,17 @@ const handlePrimaryColorChange = (event) => {
 
 useEffect(() => {
     return () => {
+        // Libérer l'URL du logo si nécessaire
         if (tempImageUrl) {
-            URL.revokeObjectURL(tempImageUrl); // Libérer l'URL de blob
+            URL.revokeObjectURL(tempImageUrl);
         }
-        else if (tempHomeImgUrl) {
-            URL.revokeObjectURL(tempHomeImgUrl);
-        }
+        // Libérer les URLs des images d'accueil
+        tempHomeImgUrls.forEach(url => {
+            URL.revokeObjectURL(url);
+        });
     };
-}, [tempImageUrl, tempHomeImgUrl]);
+}, [tempImageUrl, tempHomeImgUrls]);
+
 return (
     <Paper style={{ margin: 20, padding: 20 }}>
         <Typography variant="h4" style={{ marginBottom: 20, textAlign: 'center' }}>
@@ -126,21 +163,44 @@ return (
                 
                 <hr />
 
-                <Typography variant="h5" style={{ padding: 20, marginLeft: 20 }}>Image d'accueil</Typography>
-                <ListItem alignItems="center">
-                    {tempHomeImgUrl ? (
-                        <Box
-                            component="img"
-                            sx={{ height: 60, width: 60, marginRight: 2 }}
-                            src={tempHomeImgUrl}
-                            alt="image d'accueil"
-                            style={{ width: 100 }}
-                        />
-                    ) : (
-                        <Typography variant="body1"><div>Ajouter une image :</div></Typography>
+                <Typography variant="h5" style={{ padding: 20, marginLeft: 20 }}>Images d'accueil</Typography>
+                <ListItem alignItems="flex-start" style={{ flexDirection: 'column', marginLeft: 20 }}>
+                    {tempHomeImgUrls.length > 0 && (
+                        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '10px', marginBottom: '20px' }}>
+                            {tempHomeImgUrls.map((url, index) => (
+                                <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '5px' }}>
+                                    <Box 
+                                        component="img" 
+                                        sx={{ width: 100, height: 60 }} 
+                                        src={url} 
+                                        alt={`Prévisualisation image d'accueil ${index + 1}`} 
+                                    />
+                                    {/* Bouton pour remplacer une image spécifique */}
+                                    <label htmlFor={`replace-image-${index}`} style={{ cursor: 'pointer', display: 'block', backgroundColor: '#ddd', padding: '2px 5px', textAlign: 'center', width: '100px' }}>
+                                        Remplacer
+                                    </label>
+                                    <input 
+                                        id={`replace-image-${index}`}
+                                        type="file" 
+                                        onChange={(e) => handleFileChange(e, 'homeImgs', index)} 
+                                        style={{ display: 'none' }} 
+                                    />
+                                </div>
+                            ))}
+                        </div>
                     )}
-                    <input type="file" onChange={(e) => handleFileChange(e, 'homeImg')} />
+
+                    {/* Section pour ajouter de nouvelles images */}
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                        <Typography variant="h6" style={{ marginBottom: '10px' }}>Ajouter une nouvelle image :</Typography>
+                        <input 
+                            type="file" 
+                            onChange={(e) => handleFileChange(e, 'homeImgs')} 
+                            multiple 
+                        />
+                    </div>
                 </ListItem>
+
             <hr />
             <Typography variant="h5" style={{ padding: 20, marginLeft: 20 }} secondary="exemple: Bienvenue chez WaveConnect">Message d'accueil</Typography>
             <TextareaAutosize aria-label="minimum height" minRows={3} placeholder="exemple : Bienvenue chez WaveConnect" value={companyName} onChange={handleCompanyNameChange} style={{ marginLeft: 35, width: 300 }} />
